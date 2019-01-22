158b3f3c6e163be660cc54d81fb2a8dc29191f86
App Academy Student <aa@Apps-Mac-mini.local>
2019-01-22 11:33:37 -0500

    add users table, user model, and users controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  helper_method :current_user
+  
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
 end
