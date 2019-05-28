bbb64c00c3e1ae7e1b87e8f91b7d0ab5ee2e2df9
davidRichard23 <d2391@me.com>
2019-05-28 00:43:42 -0400

    fix error if news image doesnt exist

--- frontend/components/shared/news.jsx
+++ frontend/components/shared/news.jsx
@@ -5,20 +5,24 @@ export default ({articles}) => {
   return (
     <div className='news-container'>
       <h1 className='grey-border-bottom'>News</h1>
-      {articles.map((article, i) => (
-        <div className="news-link" key={i}>
-          <a href={article.url} target='_blank'>
-            <div className="news-image">
-              <img src={article.urlToImage.replace('http://', 'https://')} alt=""/>
-            </div>
-            <div>
-              <h4>{article.source.name}</h4>
-              <h3>{article.title}</h3>
-              <p>{article.description}</p>
-            </div>
-          </a>
-        </div>
-      ))}
+      {articles.map((article, i) => {
+        const imgUrl = article.urlToImage ? article.urlToImage.replace('http://', 'https://') : '';
+        
+        return (
+          <div className="news-link" key={i}>
+            <a href={article.url} target='_blank'>
+              <div className="news-image">
+                <img src={imgUrl} alt=""/>
+              </div>
+              <div>
+                <h4>{article.source.name}</h4>
+                <h3>{article.title}</h3>
+                <p>{article.description}</p>
+              </div>
+            </a>
+          </div>
+        )
+      })}
     </div>
   );
 };
